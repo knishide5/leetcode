8af6da144b2ebf8c47e76ba5c62345fc685f07a4
Koichi Nishide <knishide@c-fo.com>
2020-08-21 08:16:21 +0900

    fix

--- median_arrays/median_arrays.rb
+++ median_arrays/median_arrays.rb
@@ -1,7 +1,16 @@
 # @param {Integer[]} nums1
 # @param {Integer[]} nums2
 # @return {Float}
+
+# 中央値
+# 奇数の場合はちょうど真ん中にくるもの、偶数の場合は真ん中の２つの平均。
 def find_median_sorted_arrays(nums1, nums2)
   nums = nums1 + nums2
-  nums.inject(:+) / nums.length
+  sorted_nums = nums.sort_by{|x| x.to_i }
+  n = sorted_nums.length
+  if n % 2 == 0
+      (sorted_nums[n/2 - 1] + sorted_nums[n/2]) / 2.to_f
+  else
+      sorted_nums[(n-1)/2]
+  end
 end
